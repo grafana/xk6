a5ea798ce41f6ab02509dff2cd9b091c3551d30d
Iván SZKIBA <szkiba@users.noreply.github.com>
2025-06-10 07:50:56 +0000

    fix linter issue

--- cmd/xk6/main.go
+++ cmd/xk6/main.go
@@ -2,7 +2,6 @@
 // This entry point exists for compatibility reasons,
 // so that you can still run xk6 with `go run go.k6.io/xk6/cmd/xk6@latest`.
 // If you want to use `go run`, use `go run go.k6.io/xk6@latest` instead.
-
 package main
 
 import "go.k6.io/xk6/internal/cmd"
